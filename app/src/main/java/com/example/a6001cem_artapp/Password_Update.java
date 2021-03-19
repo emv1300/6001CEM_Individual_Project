2877c0c320cfea25caffadd8b55867f55faa2fa1
enem2@coventry.ac.uk <enem2@coventry.ac.uk>
2021-03-19 16:49:03 +0000

    created an empty password update activity

---
+++ app/src/main/java/com/example/a6001cem_artapp/Password_Update.java
@@ -0,0 +1,14 @@
+package com.example.a6001cem_artapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Password_Update extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password__update);
+    }
+}
