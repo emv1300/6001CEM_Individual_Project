8257676f346a2df932a87313d9306bd7fa9a7ff3
enem2@coventry.ac.uk <enem2@coventry.ac.uk>
2021-03-20 21:37:48 +0000

    Added 3 empty challenge activities

---
+++ app/src/main/java/com/example/a6001cem_artapp/Challenge3DGeometry.java
@@ -0,0 +1,14 @@
+package com.example.a6001cem_artapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Challenge3DGeometry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenge3_d_geometry);
+    }
+}
