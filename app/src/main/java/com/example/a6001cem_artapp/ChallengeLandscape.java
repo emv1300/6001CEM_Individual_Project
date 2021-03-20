749ff509d668739f27b4464f652ab1626dbf6742
enem2@coventry.ac.uk <enem2@coventry.ac.uk>
2021-03-20 23:41:41 +0000

    added functionality for Landscape random challenge

--- app/src/main/java/com/example/a6001cem_artapp/ChallengeLandscape.java
+++ app/src/main/java/com/example/a6001cem_artapp/ChallengeLandscape.java
@@ -2,13 +2,94 @@ package com.example.a6001cem_artapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class ChallengeLandscape extends AppCompatActivity {
+
+    private Button goBack, challenge;
+    private TextView weatherTV, terrainTV, vegetationTV, timeTV, structuresTV;
+    private List<String> listWeather = new ArrayList<String>(), listTerrains  = new ArrayList<String>(), listVegetation = new ArrayList<String>(), listTimeOfDay  = new ArrayList<String>(), listStructures = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challenge_landscape);
+        listWeather.addAll(Arrays.asList("sunny/no clouds","snowy","rainy","windy","cloudy","stormy","foggy","freezing","blizzard","hurricane","dust storm","flood"));
+        listTerrains.addAll(Arrays.asList("Huge Underground Cave","volcano","canyon","desert","mountains","hills","marsh","oasis","ocean","islands","coastal beach","swamp","tundra","valley","delta","glaciers","savanna"));
+        listVegetation.addAll(Arrays.asList("arid","desolate","mountain forrest","arctic forest","tropical forest", "mushroom forest", "laurel forests", "rain forest", "jungle", "open","grass land","mountain forest","barren","steppe","no vegetation","cactus forest","a shrubbery, Ni!"));
+        listTimeOfDay.addAll(Arrays.asList("morning","dawn","sunrise","midday","afternoon","sunset","dusk","night","full moon","half moon","eclipse"));
+        listStructures.addAll(Arrays.asList("--","--","--","--","--","--","castle","manor","villa","village","cottage","cabin","camp","arena","temple"));
+        goBack = findViewById(R.id.goBackChallengeLandscapeBT);
+        challenge = findViewById(R.id.LChallengeBT);
+        structuresTV = findViewById(R.id.structuresTV);
+        weatherTV = findViewById(R.id.tvWord1Landscape);
+        terrainTV = findViewById(R.id.tvWord4Landscape);
+        vegetationTV = findViewById(R.id.tvWord3Landscape);
+        timeTV = findViewById(R.id.tvWord2Landscape);
+
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =  new Intent(ChallengeLandscape.this, ChallengeNavigation.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        challenge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<Integer> numList = new ArrayList<Integer>();
+                numList.add(new Random().nextInt(listWeather.size()));
+                numList.add(new Random().nextInt(listTerrains.size()));
+                numList.add(new Random().nextInt(listVegetation.size()));
+                numList.add(new Random().nextInt(listTimeOfDay.size()));
+                numList.add(new Random().nextInt(listStructures.size()));
+
+                for(int i = 0; i < 4; i++){
+                    if(i == 0 ){
+                        for(int j = 0; j < listWeather.size(); j++){
+                            if (numList.get(i) == j){
+                                weatherTV.setText(listWeather.get(j));
+                            }
+                        }
+                    }else if(i==1){
+                        for(int j = 0; j < listTerrains.size(); j++){
+                            if (numList.get(i) == j){
+                                terrainTV.setText(listTerrains.get(j));
+                            }
+                        }
+                    }else if (i==2){
+                        for(int j = 0; j < listVegetation.size(); j++){
+                            if (numList.get(i) == j){
+                                vegetationTV.setText(listVegetation.get(j));
+                            }
+                        }
+                    }else if (i==3){
+                        for(int j = 0; j < listTimeOfDay.size(); j++){
+                            if (numList.get(i) == j){
+                                timeTV.setText(listTimeOfDay.get(j));
+                            }
+                        }
+                    }else {
+                        for(int j = 0; j < listStructures.size(); j++){
+                            if (numList.get(i) == j){
+                                timeTV.setText(listStructures.get(j));
+                            }
+                        }
+                    }
+                }
+            }
+        });
     }
 }
