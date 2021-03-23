a80db1848cdcf898dc44a6a1fac28312189991a9
enem2@coventry.ac.uk <enem2@coventry.ac.uk>
2021-03-23 22:37:36 +0000

    created empty user guides activity

---
+++ app/src/main/java/com/example/a6001cem_artapp/UserGuides.java
@@ -0,0 +1,14 @@
+package com.example.a6001cem_artapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserGuides extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_guides);
+    }
+}
