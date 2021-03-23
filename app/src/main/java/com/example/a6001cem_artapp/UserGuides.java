394127e9f0790d57d67449537e98993c43f6593e
enem2@coventry.ac.uk <enem2@coventry.ac.uk>
2021-03-23 22:51:24 +0000

    added functionality for displaying user guides meant to explain the daily challenge and random challenge generator

--- app/src/main/java/com/example/a6001cem_artapp/UserGuides.java
+++ app/src/main/java/com/example/a6001cem_artapp/UserGuides.java
@@ -2,13 +2,38 @@ package com.example.a6001cem_artapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class UserGuides extends AppCompatActivity {
+
+    private String userGuide;
+    private TextView dailyGuideTV;
+    private ScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_guides);
+        dailyGuideTV = findViewById(R.id.dailyChallengeUserGuideTv);
+        scrollView = findViewById(R.id.scrollViewRandom);
+
+        Intent intent = getIntent();
+        userGuide = ""+ intent.getStringExtra("UserGuide");
+
+
+        if (userGuide.equals("show daily guide")){
+            dailyGuideTV.setVisibility(View.VISIBLE);
+            scrollView.setVisibility(View.INVISIBLE);
+        }else if (userGuide.equals("show random guide")){
+
+            dailyGuideTV.setVisibility(View.INVISIBLE);
+
+            scrollView.setVisibility(View.VISIBLE);
+        }
+
     }
 }
