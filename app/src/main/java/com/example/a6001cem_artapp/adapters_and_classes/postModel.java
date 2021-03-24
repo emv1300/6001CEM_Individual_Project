0855408e661f8f1b3649feb9a36afd911bb8460d
enem2@coventry.ac.uk <enem2@coventry.ac.uk>
2021-03-24 18:30:19 +0000

    created a model class for uploaded user posts with constructor and getters

--- app/src/main/java/com/example/a6001cem_artapp/adapters_and_classes/postModel.java
+++ app/src/main/java/com/example/a6001cem_artapp/adapters_and_classes/postModel.java
@@ -1,4 +1,68 @@
 package com.example.a6001cem_artapp.adapters_and_classes;
 
 public class postModel {
+
+    private String postCommentsNum, postID, postTitle, postDescription, postImage, postTimestamp, userID, userPfp, userName, postLikes, reportsNum;
+
+    public  postModel(){
+
+    }
+
+    public postModel(String postCommentsNum, String postID, String postTitle, String postDescription, String postImage, String postTimestamp, String userID, String userPfp, String userName, String postLikes, String reportsNum) {
+        this.postCommentsNum = postCommentsNum;
+        this.postID = postID;
+        this.postTitle = postTitle;
+        this.postDescription = postDescription;
+        this.postImage = postImage;
+        this.postTimestamp = postTimestamp;
+        this.userID = userID;
+        this.userPfp = userPfp;
+        this.userName = userName;
+        this.postLikes = postLikes;
+        this.reportsNum = reportsNum;
+    }
+
+    public String getPostCommentsNum() {
+        return postCommentsNum;
+    }
+
+    public String getPostID() {
+        return postID;
+    }
+
+    public String getPostTitle() {
+        return postTitle;
+    }
+
+    public String getPostDescription() {
+        return postDescription;
+    }
+
+    public String getPostImage() {
+        return postImage;
+    }
+
+    public String getPostTimestamp() {
+        return postTimestamp;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getUserPfp() {
+        return userPfp;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPostLikes() {
+        return postLikes;
+    }
+
+    public String getReportsNum() {
+        return reportsNum;
+    }
 }
