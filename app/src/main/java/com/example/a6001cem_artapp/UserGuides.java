f992024de4337d3d2ccdb11d0339fa750b30af0b
enem2@coventry.ac.uk <enem2@coventry.ac.uk>
2021-03-24 21:10:44 +0000

    deleted all but one tv guide so that the activity loads the text from firebase into the remaining tv

--- app/src/main/java/com/example/a6001cem_artapp/UserGuides.java
+++ app/src/main/java/com/example/a6001cem_artapp/UserGuides.java
@@ -1,5 +1,6 @@
 package com.example.a6001cem_artapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,32 +9,48 @@ import android.view.View;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class UserGuides extends AppCompatActivity {
 
     private String userGuide;
-    private TextView dailyGuideTV;
-    private ScrollView scrollView;
+    private TextView userGuideTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_guides);
-        dailyGuideTV = findViewById(R.id.dailyChallengeUserGuideTv);
-        scrollView = findViewById(R.id.scrollViewRandom);
 
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
+        userGuideTV = findViewById(R.id.dailyChallengeUserGuideTv);
         Intent intent = getIntent();
         userGuide = ""+ intent.getStringExtra("UserGuide");
 
 
-        if (userGuide.equals("show daily guide")){
-            dailyGuideTV.setVisibility(View.VISIBLE);
-            scrollView.setVisibility(View.INVISIBLE);
-        }else if (userGuide.equals("show random guide")){
+        dbRef.child("UserGuide").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (userGuide.equals("show daily guide")){
+                    String dailyGuideText = snapshot.child("dailyChallenge").getValue().toString();
+                    if (dailyGuideText!=null){
+                    userGuideTV.setText(dailyGuideText);
+                    }
+                }else if (userGuide.equals("show random guide")){
+                    String randomGuideText = snapshot.child("randomChallenge").getValue().toString();
+                    if (randomGuideText!=null) {
+                        userGuideTV.setText(randomGuideText);
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
 
-            dailyGuideTV.setVisibility(View.INVISIBLE);
-
-            scrollView.setVisibility(View.VISIBLE);
-        }
+            }
+        });
 
     }
 }
