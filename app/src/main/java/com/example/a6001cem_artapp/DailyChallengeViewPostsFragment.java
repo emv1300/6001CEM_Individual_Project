5f179e07f63f6d32c45330390090f99e74d61969
enem2@coventry.ac.uk <enem2@coventry.ac.uk>
2021-03-24 18:43:41 +0000

    added functionality for viewing/searching for posts and for refreshing recyclerview along with loaded posts

--- app/src/main/java/com/example/a6001cem_artapp/DailyChallengeViewPostsFragment.java
+++ app/src/main/java/com/example/a6001cem_artapp/DailyChallengeViewPostsFragment.java
@@ -2,63 +2,235 @@ package com.example.a6001cem_artapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.SearchView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DailyChallengeViewPostsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.a6001cem_artapp.adapters_and_classes.postModel;
+import com.example.a6001cem_artapp.adapters_and_classes.postAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class DailyChallengeViewPostsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private RecyclerView recyclerView;
+    private List<postModel> postList;
+    private postAdapter postAdapter;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     public DailyChallengeViewPostsFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DailyChallengeViewPostsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DailyChallengeViewPostsFragment newInstance(String param1, String param2) {
-        DailyChallengeViewPostsFragment fragment = new DailyChallengeViewPostsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_daily_challenge_view_posts, container, false);
+        View view = inflater.inflate(R.layout.fragment_daily_challenge_view_posts, container, false);
+
+        swipeRefreshLayout = view.findViewById(R.id.swipePostsRefresh);
+        recyclerView = view.findViewById(R.id.postsRV);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        layoutManager.setStackFromEnd(true);
+        layoutManager.setReverseLayout(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+        postList = new ArrayList<>();
+
+        loadAllPosts();
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                loadAllPosts();
+                if(swipeRefreshLayout.isRefreshing()) {
+
+                    swipeRefreshLayout.setRefreshing(false);
+                }
+            }
+        });
+        return view;}
+
+    private void loadAllPosts() {
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("Posts");
+        dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                postList.clear();
+                for(DataSnapshot ds: snapshot.getChildren()){
+                    postModel postModel = ds.getValue(postModel.class);
+                    String numberOfReports = ds.child("reportsNum").getValue().toString();
+                    if (ds.child("postImage").getValue() != null && Integer.parseInt(numberOfReports)<3) {
+                        postList.add(postModel);
+                    }
+                    postAdapter = new postAdapter(getActivity(), postList);
+                    recyclerView.setAdapter(postAdapter);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+    private void searchOwnPosts(){
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("Posts");
+        dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                postList.clear();
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    if (ds.child("postImage").getValue()!=null){
+                        postModel postModel = ds.getValue(postModel.class);
+                        String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                        if(postModel.getUserID().contains(userID))
+                        { postList.add(postModel);}}
+                    postAdapter = new postAdapter(getActivity(), postList);
+                    recyclerView.setAdapter(postAdapter);
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getActivity(), "" + error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+    private void searchSpecificPosts(final String sQ) {
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("Posts");
+        dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                postList.clear();
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    if (ds.child("postImage").getValue()!=null) {
+                        postModel postModel = ds.getValue(postModel.class);
+                        String numberOfReports = ds.child("reportsNum").getValue().toString();
+                        if (Integer.parseInt(numberOfReports)< 2 && (postModel.getPostTitle().toLowerCase().contains(sQ.toLowerCase())
+                                || postModel.getPostDescription().toLowerCase().contains(sQ.toLowerCase()) || postModel.getUserName().toLowerCase().contains(sQ.toLowerCase()))) {
+                            postList.add(postModel);
+                        }
+                    }
+                    postAdapter = new postAdapter(getActivity(), postList);
+                    recyclerView.setAdapter(postAdapter);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getActivity(), "" + error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void mostLikedPosts(){
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("Posts");
+        dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                postList.clear();
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    if(ds.child("postImage").getValue()!=null) {
+                        postModel postModel = ds.getValue(postModel.class);
+                        String numberOfReports = ds.child("reportsNum").getValue().toString();
+                        if (Integer.parseInt(numberOfReports)< 2 && (postModel.getPostLikes().contains("25") || postModel.getPostLikes().contains("26")
+                                || postModel.getPostLikes().contains("27") || postModel.getPostLikes().contains("28") ||
+                                postModel.getPostLikes().contains("29") || postModel.getPostLikes().contains("30"))) {
+                            postList.add(postModel);
+                        }
+                    }
+                    postAdapter = new postAdapter(getActivity(), postList);
+                    recyclerView.setAdapter(postAdapter);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getActivity(), "" + error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+
+        inflater.inflate(R.menu.menu_view_posts, menu);
+        MenuItem item1 = menu.findItem(R.id.search_posts);
+        SearchView searchView = (SearchView) item1.getActionView();
+        MenuItem item2 = menu.findItem(R.id.ViewOwnPosts);
+        ImageButton ownPosts = (ImageButton) item2.getActionView();
+        MenuItem item3 = menu.findItem(R.id.ViewBestPosts);
+        ImageButton allPosts = (ImageButton) item3.getActionView();
+        item2.setIcon(R.drawable.ic_my_posts);
+        item3.setIcon(R.drawable.ic_like_on);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //called when the user presses search bt
+                if (!TextUtils.isEmpty(query)){
+                    searchSpecificPosts(query);
+                }else{
+                    loadAllPosts();
+                }
+                return false;
+
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                if (!TextUtils.isEmpty(newText)){
+                    searchSpecificPosts(newText);
+                }else{
+                    loadAllPosts();
+                }
+                return false;
+            }
+        });
+        ownPosts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchOwnPosts();
+            }
+        });
+        allPosts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               mostLikedPosts();
+            }
+        });
+
+        super.onCreateOptionsMenu(menu, inflater);
     }
 }
