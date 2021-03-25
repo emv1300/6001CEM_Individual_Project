b7a85ebf7ed967143572100cee90e9dedd15b123
enem2@coventry.ac.uk <enem2@coventry.ac.uk>
2021-03-25 16:46:16 +0000

    added class variables, constructor and getters

--- app/src/main/java/com/example/a6001cem_artapp/adapters_and_classes/commentModel.java
+++ app/src/main/java/com/example/a6001cem_artapp/adapters_and_classes/commentModel.java
@@ -1,4 +1,38 @@
 package com.example.a6001cem_artapp.adapters_and_classes;
 
 public class commentModel {
+
+    private String commentID, commentTimeStamp, commentText, userId, userName;
+
+    public commentModel(){
+
+    }
+
+    public commentModel(String commentID, String commentTimeStamp, String commentText, String userId, String userName) {
+        this.commentID = commentID;
+        this.commentTimeStamp = commentTimeStamp;
+        this.commentText = commentText;
+        this.userId = userId;
+        this.userName = userName;
+    }
+
+    public String getCommentID() {
+        return commentID;
+    }
+
+    public String getCommentTimeStamp() {
+        return commentTimeStamp;
+    }
+
+    public String getCommentText() {
+        return commentText;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
 }
